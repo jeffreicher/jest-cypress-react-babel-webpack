961f761554eec979d9b25f49ff4a112752937376
Jeffrei Cher <jeffreicher@yahoo.com>
2018-11-12 10:16:50 -0800

    Added assertion

--- src/shared/__tests__/super-heros.js
+++ src/shared/__tests__/super-heros.js
@@ -2,10 +2,10 @@ import {getFlyingSuperHeros} from '../super-heros'
 
 test('returns super heros that can fly', () => {
   const flyingHeros = getFlyingSuperHeros()
-  console.log(flyingHeros)
-  expect(flyingHeros).toEqual([
-    {name: 'Dynaguy', powers: ['disintegration ray', 'fly']},
-    {name: 'Apogee', powers: ['gravity control', 'fly']},
-    {name: 'Jack-Jack', powers: ['shapeshifting', 'fly']},
-  ])
+  expect(flyingHeros).toMatchSnapshot()
+  //   expect(flyingHeros).toEqual([
+  //     {name: 'Dynaguy', powers: ['disintegration ray', 'fly']},
+  //     {name: 'Apogee', powers: ['gravity control', 'fly']},
+  //     {name: 'Jack-Jack', powers: ['shapeshifting', 'fly']},
+  //   ])
 })
