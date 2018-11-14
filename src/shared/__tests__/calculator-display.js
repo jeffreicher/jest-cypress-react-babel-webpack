4b501e642d5369476461fa97bd9dfcfaa0c49309
Jeffrei Cher <jeffreicher@yahoo.com>
2018-11-14 12:10:01 -0800

    moved reuseable function to seperate file for export and set up jest module directory test path

--- src/shared/__tests__/calculator-display.js
+++ src/shared/__tests__/calculator-display.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import {render} from 'react-testing-library'
 import CalculatorDisplay from '../calculator-display'
+import {render} from 'calculator-test-utils'
 
 test('mounts', () => {
   const {container} = render(<CalculatorDisplay value="0" />)
