c4b8883b1c0df8b777473f079f416308bf75b1f9
Jeffrei Cher <jeffreicher@yahoo.com>
2018-11-29 13:27:40 -0800

    Added codecov to travis

--- jest.config.js
+++ jest.config.js
@@ -16,4 +16,19 @@ module.exports = {
   //after Jest is loaded
   setupTestFrameworkScriptFile: require.resolve('./test/setup-test.js'),
   collectCoverageFrom: ['**/src/**/*.js'],
+  coverageThreshold: {
+    global: {
+      statements: 17,
+      branches: 4,
+      lines: 17,
+      functions: 20,
+    },
+    //glob to set threshold for specific files
+    './src/shared/utils.js': {
+      statements: 100,
+      branches: 80,
+      lines: 100,
+      functions: 100,
+    },
+  },
 }
